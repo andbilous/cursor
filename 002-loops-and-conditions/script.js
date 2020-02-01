34a7a1d03f443768f3d605d8b17997af0907f088
andbilous <andbilous@gmail.com>
2020-02-01 22:40:51 +0200

    homework#2-2

--- 002-loops-and-conditions/script.js
+++ 002-loops-and-conditions/script.js
@@ -1,1 +1,25 @@
-console.log('Loops and conditions');
+let m  = NaN;
+while (isNaN(m)) {
+  m = parseInt(prompt("Enter M", ''));
+}
+let n  = NaN;
+while (isNaN(n)) {
+  n = parseInt(prompt("Enter N", ''));
+}
+
+let skipEven = confirm("Пропустити парні ?");
+
+document.write('M : ',m+'<br \/>');
+document.write('N : ',n+'<br \/>');
+
+let sum=0;
+
+for(let i=m; i<=n; i++){
+  if (skipEven){
+    if (i%2==0) {
+      continue;
+    }
+  }
+  sum+=i;
+}
+document.write('Sum :: ',sum);
