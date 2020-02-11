ea5dc646ea1343f8adf453c31d7cd4e2d93f6766
andbilous-digis <andbilous@gmail.com>
2020-02-11 10:36:21 +0200

    fix2

--- 003-functions/script.js
+++ 003-functions/script.js
@@ -1,9 +1,9 @@
-const getMaxDigit = (number) => {
+const getMaxDigit = (number) => {   // Макс число
   const sortedArray = Array.from(String(number), Number).sort();
   return sortedArray[sortedArray.length-1];
 };
 
-const getNumberPower = (number,power) => {
+const getNumberPower = (number,power) => {   // Степінь
   let res=1;
   for(let i =0; i<power; i++){
     res*=number;
@@ -11,32 +11,36 @@ const getNumberPower = (number,power) => {
   return res;
 };
 
-const makeFirstLetterTitle = (word) => {
-  let res = word.split('')
+const makeFirstLetterTitle = (word) => {   // Перша буква велика
+  let lowerCasedWord = word.toLowerCase();
+  let res = lowerCasedWord.split('');
   res[0] = res[0].toUpperCase();
   return res.join('');
 };
 
-const getRandomNumber = (n,m) => {
+const getRandomNumber = (n,m) => {        // Випадкове число
   return Math.floor(Math.random() * (m - n) + n);
 };
 
-const  removeLettersFromSentence = (sentence,letter) => {
+const  removeLettersFromSentence = (sentence,letter) => {  // Видалити з речення
   let pattern =  new RegExp(letter, 'g');
   return sentence.replace(pattern, '');
 }
 
-const isPalindrome = (word) => {
+const isPalindrome = (word) => {    //  Паліндром
   if(!word){
     return false;
   }
- return word.includes(word.split('').reverse().join(''))
+  let singleLowerCasedWord = word.toLowerCase().split(' ').join('');
+ return singleLowerCasedWord.includes(singleLowerCasedWord.split('').reverse().join(''))
 }
 
 console.log(getMaxDigit(123567));
 console.log(getNumberPower(5,2));
-console.log(makeFirstLetterTitle('hello'));
+console.log(makeFirstLetterTitle('влад'));
+console.log(makeFirstLetterTitle('вЛАД'));
 console.log(getRandomNumber(1,10));
 console.log(removeLettersFromSentence('hello gaga aaasswaa','a'));
-console.log(isPalindrome('madam'));
-console.log(isPalindrome('mader'));
+console.log(isPalindrome('мадам'));
+console.log(isPalindrome('кокос'));
+console.log(isPalindrome('Аргентина манит негра'));
