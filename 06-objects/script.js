98cb4bf292e6a93b82117dbc328543823a5aad6b
andbilous-digis <andbilous@gmail.com>
2020-02-16 10:47:14 +0200

    dz

--- 06-objects/script.js
+++ 06-objects/script.js
@@ -1,51 +1,94 @@
-const students = ["Саша", "Игорь", "Лена", "Ира", "Алексей", "Светлана"];
-const themes = ["Дифференциальные уравнения", "Теория автоматов", "Алгоритмы и структуры данных"];
-const marks = [4, 5, 5, 3, 4, 5];
+const students = [{
+  name: "Tanya",
+  course: 3,
+  subjects: {
+    math: [4, 4, 3, 4],
+    algorithms: [3, 3, 3, 4, 4, 4],
+    data_science: [5, 5, 3, 4]
+  }
+}, {
+  name: "Victor",
+  course: 4,
+  subjects: {
+    physics: [5, 5, 5, 3],
+    economics: [2, 3, 3, 3, 3, 5],
+    geometry: [5, 5, 2, 3, 5]
+  }
+}, {
+  name: "Anton",
+  course: 2,
+  subjects: {
+    statistics: [4, 5, 5, 5, 5, 3, 4, 3, 4, 5],
+    english: [5, 3],
+    cosmology: [5, 5, 5, 5]
+  }
+}];
 
-const splitIntoPairs = (students) => {
-  let res = [];
-  res.push([students[0],students[2]]);
-  res.push([students[1],students[3]]);
-  res.push([students[4],students[5]]);
-  return res;
-};
-
-const addSubjectToStudentsPair = (pairs) => {
-  return pairs.map((pair,index)=> {
-    let transformedPair=[];
-    let pairName = pair.join(' и ');
-    transformedPair.push(pairName);
-    transformedPair.push(themes[index]);
-    return transformedPair;
-  });
-};
-
-const addMarks = (students,marks) =>{
-  let res = [];
- students.forEach( (student,index)=> {
-    let studentWithMark = [];
-    studentWithMark.push(student);
-    studentWithMark.push(marks[index]);
-    res.push(studentWithMark);
-  });
-  return res;
-};
-
-let pairs = splitIntoPairs(students);
-
-let pairsWithSubjects = addSubjectToStudentsPair(pairs);
-
-
-const setMarksToPairs = () => {
-   return pairsWithSubjects.map((pair)=> {
-      pair.push(marks[Math.floor(Math.random() * 5) + 1]);
-      return pair;
+const getSubjects = (student) => {
+    return  Object.keys(student.subjects).map((subject)=>{
+      return subject.charAt(0).toUpperCase() + subject.slice(1)
     });
 };
 
 
+const getAverageMark = (student) => {
+  let allMarks = Object.values(student.subjects).flat();
+  return (allMarks.reduce((accumulator, currentValue)=>{
+    return accumulator + currentValue;
+  })/allMarks.length).toFixed(2);
+};
 
-console.log(splitIntoPairs(students));
-console.log(addSubjectToStudentsPair(pairs));
-console.log(addMarks(students,marks));
-console.log(setMarksToPairs());
+const getStudentInfo = (student) => {
+  return {
+    course: student.course,
+    name: student.name,
+    averageMark: getAverageMark(student)
+  }
+};
+
+const getStudentsNames = (students) => {
+  let names=[];
+    students.forEach((student=>{
+      names.push(student.name);
+    }))
+    return names.sort();
+};
+
+
+const getBestStudent = (students) => {
+  let bestStudent = '';
+  let bestMark = 0;
+  students.forEach(student => {
+   let averageMark= getAverageMark(student)
+      if(averageMark >= bestMark){
+        bestMark=averageMark;
+        bestStudent=student.name;
+      }
+  })
+  return bestStudent;
+
+};
+
+
+const calculateWordLetters = (word) => {
+  let result={};
+    let wordInArray = word.split('');
+    wordInArray.forEach(letter=>{
+      result[letter]=0;
+    });
+
+   wordInArray.forEach(letter =>{
+      result[letter]++;
+   });
+   
+   return result;
+};
+
+
+
+console.log(getSubjects(students[0]));
+console.log(getAverageMark(students[0]));
+console.log(getStudentInfo(students[0]));
+console.log(getStudentsNames(students));
+console.log(getBestStudent(students));
+console.log(calculateWordLetters('тест'));
