8e9ddae59ca88a348b9164d46159465cedcffec6
andbilous-digis <andbilous@gmail.com>
2020-04-01 10:58:11 +0300

    fix2

--- 13-generators/script.js
+++ 13-generators/script.js
@@ -8,7 +8,7 @@ console.log(idGenerator.next().value)
 console.log(idGenerator.next().value)
 console.log(idGenerator.next().value)
 
-export function* newFontGenerator(i) {
+ function* newFontGenerator(i) {
   while (true) {
     let currentValue = yield i;
     if (currentValue === "up") {
